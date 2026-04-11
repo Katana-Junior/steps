62d32632f3425b18046a6b3f1d20de887da91238
John Katana <johnkatana624@gmail.com>
2026-04-11 19:24:30 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+const messages = [
+  "Learn React",
+  "Apply for jobs💼",
+  "Invest your new income wisely🏠",
+];
 function App() {
+  const step = 1;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="steps">
+        <div className="numbers">
+          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        </div>
+        <p className="messages">{messages[step - 1]}</p>
+        <div className="buttons">
+          <button style={{ color: "#fff", backgroundColor: "#7950f2" }}>
+            Previous
+          </button>
+          <button style={{ color: "#fff", backgroundColor: "#7950f2" }}>
+            Next
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
